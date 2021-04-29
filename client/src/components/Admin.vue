688553bc62be5213b6ebc68caafdefc2ef5abdcd
IakMastro <cse242017102@uniwa.gr>
2021-04-29 19:01:38 +0300

    Fixed wrong tag on admin container

--- client/src/components/Admin.vue
+++ client/src/components/Admin.vue
@@ -27,7 +27,7 @@
           <tr v-for="(game, index) in games" :key="index">
             <td>{{ game.title }}</td>
             <td>{{ game.developer }}</td>
-           <td>{{ game.genre }}</td>
+            <td>{{ game.genre }}</td>
             <td>
               <div class="btn-group" role="group">
                 <button type="button"
@@ -242,8 +242,8 @@ export default {
 
 <style scoped>
 
-tr{
-background-color:white;
+tr {
+  background-color: white;
 
 }
 
